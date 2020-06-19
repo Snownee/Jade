ad9f8c64914a13a7366243776b9e3eff65d24fd2
Snownee <1850986885@qq.com>
2020-06-19 08:44:32 +0800

    Another server error

--- src/main/java/snownee/jade/JadeClientPlugin.java
+++ src/main/java/snownee/jade/JadeClientPlugin.java
@@ -4,18 +4,19 @@ import mcp.mobius.waila.api.IRegistrar;
 import mcp.mobius.waila.api.IWailaPlugin;
 import mcp.mobius.waila.api.WailaPlugin;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.DistExecutor;
 import snownee.jade.client.renderer.BoxTooltipRenderer;
 import snownee.jade.client.renderer.StringTooltipRenderer;
 
-@OnlyIn(Dist.CLIENT)
 @WailaPlugin
 public class JadeClientPlugin implements IWailaPlugin {
 
     @Override
     public void register(IRegistrar registrar) {
-        registrar.registerTooltipRenderer(Renderables.BORDER, new BoxTooltipRenderer());
-        registrar.registerTooltipRenderer(Renderables.OFFSET_TEXT, new StringTooltipRenderer());
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
+            registrar.registerTooltipRenderer(Renderables.BORDER, new BoxTooltipRenderer());
+            registrar.registerTooltipRenderer(Renderables.OFFSET_TEXT, new StringTooltipRenderer());
+        });
     }
 
 }
