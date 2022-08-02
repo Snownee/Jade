b8bf950b94c21f5ee3b72b35fcf8f0c361e84b4f
Snownee <i@snownee.com>
2022-08-03 02:22:57 +0800

    feat: make null be a possible value in JadeRayTraceCallback

--- src/main/java/snownee/jade/JadeClient.java
+++ src/main/java/snownee/jade/JadeClient.java
@@ -143,11 +143,11 @@ public final class JadeClient {
 
 	@Nullable
 	public static Accessor<?> builtInOverrides(HitResult hitResult, @Nullable Accessor<?> accessor, @Nullable Accessor<?> originalAccessor) {
-		Player player = accessor.getPlayer();
-		if (player.isCreative() || player.isSpectator())
-			return accessor;
-		IWailaClientRegistration client = VanillaPlugin.CLIENT_REGISTRATION;
 		if (accessor instanceof BlockAccessor target) {
+			Player player = accessor.getPlayer();
+			if (player.isCreative() || player.isSpectator())
+				return accessor;
+			IWailaClientRegistration client = VanillaPlugin.CLIENT_REGISTRATION;
 			if (target.getBlock() instanceof TrappedChestBlock) {
 				BlockState state = VanillaPlugin.getCorrespondingNormalChest(target.getBlockState());
 				if (state != target.getBlockState()) {
