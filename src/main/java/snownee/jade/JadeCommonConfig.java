7f887d1b8dae37130686af31d3bc68e71a143d14
Snownee <i@snownee.com>
2023-01-26 00:51:43 +0800

    improve: use less hardcode

--- src/main/java/snownee/jade/JadeCommonConfig.java
+++ src/main/java/snownee/jade/JadeCommonConfig.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import com.google.common.collect.Sets;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import snownee.jade.util.PlatformProxy;
 
@@ -64,7 +65,7 @@ public final class JadeCommonConfig {
 	public static boolean shouldShowCustomName(BlockEntity t) {
 		String modid = PlatformProxy.getId(t.getType()).getNamespace();
 		if (onlyShowVanilla) {
-			return "minecraft".equals(modid);
+			return ResourceLocation.DEFAULT_NAMESPACE.equals(modid);
 		} else {
 			return !modBlacklist.contains(modid);
 		}
