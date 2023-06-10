d1691c4c891773d63422ea2aa656afbb2bb7c682
Snownee <i@snownee.com>
2023-06-10 23:21:16 +0800

    fix: #252

--- src/main/java/snownee/jade/addon/vanilla/HorseStatsProvider.java
+++ src/main/java/snownee/jade/addon/vanilla/HorseStatsProvider.java
@@ -19,15 +19,16 @@ public enum HorseStatsProvider implements IEntityComponentProvider {
 	@Override
 	public void appendTooltip(ITooltip tooltip, EntityAccessor accessor, IPluginConfig config) {
 		AbstractHorse horse = (AbstractHorse) accessor.getEntity();
-		double jumpStrength = horse.getAttributeValue(Attributes.JUMP_STRENGTH);
+		if (horse instanceof Llama llama) {
+			tooltip.add(Component.translatable("jade.llamaStrength", llama.getStrength()));
+			return;
+		}
+		double jumpStrength = horse.getCustomJump();
 		double jumpHeight = -0.1817584952 * jumpStrength * jumpStrength * jumpStrength + 3.689713992 * jumpStrength * jumpStrength + 2.128599134 * jumpStrength - 0.343930367;
 		// https://minecraft.fandom.com/wiki/Horse?so=search#Movement_speed
 		double speed = horse.getAttributeValue(Attributes.MOVEMENT_SPEED) * 43.17;
 		tooltip.add(Component.translatable("jade.horseStat.jump", DisplayHelper.dfCommas.format(jumpHeight)));
 		tooltip.add(Component.translatable("jade.horseStat.speed", DisplayHelper.dfCommas.format(speed)));
-		if (horse instanceof Llama llama) {
-			tooltip.add(Component.translatable("jade.llamaStrength", llama.getStrength()));
-		}
 	}
 
 	@Override
