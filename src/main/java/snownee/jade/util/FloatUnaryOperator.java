b1e5cdcfe0c427461499c8ec29bc31ea716ed43b
Curodenz <2406795813@qq.com>
2023-07-14 13:21:25 +0800

    add the FloatUnaryOperator, which is missing in fastutil 8.2.1

--- src/main/java/snownee/jade/util/FloatUnaryOperator.java
+++ src/main/java/snownee/jade/util/FloatUnaryOperator.java
@@ -1,33 +1,31 @@
 package snownee.jade.util;
 
-import it.unimi.dsi.fastutil.SafeMath;
-import java.util.function.DoubleUnaryOperator;
 import java.util.function.UnaryOperator;
+
+/**
+ * A depended class that not exist in fastutil 8.2.1
+ * This impl is from fastutil 8.2.12
+ */
 @FunctionalInterface
-public interface FloatUnaryOperator extends UnaryOperator<Float>, DoubleUnaryOperator {
-    float apply(float var1);
+public interface FloatUnaryOperator extends UnaryOperator<Float>, java.util.function.DoubleUnaryOperator {
+	float apply(float x);
 
-    static FloatUnaryOperator identity() {
-        return (i) -> {
-            return i;
-        };
-    }
+	public static FloatUnaryOperator identity() {
+		return  i -> i;
+	}
 
-    static FloatUnaryOperator negation() {
-        return (i) -> {
-            return -i;
-        };
-    }
+	public static FloatUnaryOperator negation() { return i -> -i; }
 
-    /** @deprecated */
-    @Deprecated
-    default double applyAsDouble(double x) {
-        return (double)this.apply(SafeMath.safeDoubleToFloat(x));
-    }
+	@Deprecated
+	@Override
+	default double applyAsDouble(final double x) {
+		return apply(it.unimi.dsi.fastutil.SafeMath.safeDoubleToFloat(x));
+	}
 
-    /** @deprecated */
-    @Deprecated
-    default Float apply(Float x) {
-        return this.apply(x);
-    }
+	@Deprecated
+	@Override
+	@SuppressWarnings("boxing")
+	default Float apply(final Float x) {
+		return apply(x.floatValue());
+	}
 }
