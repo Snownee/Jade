34875d19cd2ece01e2c5c18851a50d6eb7139ba5
Snownee <i@snownee.com>
2023-08-01 17:39:16 +0800

    improve: tweaks to HorseStatsProvider
     - now shows max values when sneaking
     - shows base value rather than modified value

--- src/main/java/snownee/jade/addon/vanilla/HorseStatsProvider.java
+++ src/main/java/snownee/jade/addon/vanilla/HorseStatsProvider.java
@@ -18,23 +18,40 @@ public enum HorseStatsProvider implements IEntityComponentProvider {
 
 	INSTANCE;
 
+	private static final double MAX_JUMP_HEIGHT = getJumpHeight(AbstractHorse.MAX_JUMP_STRENGTH);
+	private static final double MAX_MOVEMENT_SPEED = AbstractHorse.MAX_MOVEMENT_SPEED * 42.16;
+
+	private static Component switchText(String key, boolean showMax, double value, double max) {
+		IThemeHelper t = IThemeHelper.get();
+		Component valueText = t.info(DisplayHelper.dfCommas.format(value));
+		if (showMax) {
+			return Component.translatable(key, Component.translatable("jade.fraction", valueText, DisplayHelper.dfCommas.format(max)));
+		} else {
+			return Component.translatable(key, valueText);
+		}
+	}
+
+	private static double getJumpHeight(double jumpStrength) {
+		return -0.1817584952 * jumpStrength * jumpStrength * jumpStrength + 3.689713992 * jumpStrength * jumpStrength + 2.128599134 * jumpStrength - 0.343930367;
+	}
+
 	@Override
 	public void appendTooltip(ITooltip tooltip, EntityAccessor accessor, IPluginConfig config) {
 		AbstractHorse horse = (AbstractHorse) accessor.getEntity();
-		IThemeHelper t = IThemeHelper.get();
+		boolean showMax = accessor.showDetails();
 		if (horse instanceof Llama llama) {
-			tooltip.add(Component.translatable("jade.llamaStrength", t.info(llama.getStrength())));
+			tooltip.add(switchText("jade.llamaStrength", showMax, llama.getStrength(), 5));
 			return;
 		}
 		if (horse instanceof Camel) {
 			return;
 		}
-		double jumpStrength = horse.getCustomJump();
-		double jumpHeight = -0.1817584952 * jumpStrength * jumpStrength * jumpStrength + 3.689713992 * jumpStrength * jumpStrength + 2.128599134 * jumpStrength - 0.343930367;
+		double jumpStrength = horse.getAttributeBaseValue(Attributes.JUMP_STRENGTH);
+		double jumpHeight = getJumpHeight(jumpStrength);
 		// https://minecraft.fandom.com/wiki/Horse?so=search#Movement_speed
-		double speed = horse.getAttributeValue(Attributes.MOVEMENT_SPEED) * 42.16;
-		tooltip.add(Component.translatable("jade.horseStat.jump", t.info(DisplayHelper.dfCommas.format(jumpHeight))));
-		tooltip.add(Component.translatable("jade.horseStat.speed", t.info(DisplayHelper.dfCommas.format(speed))));
+		double speed = horse.getAttributeBaseValue(Attributes.MOVEMENT_SPEED) * 42.16;
+		tooltip.add(switchText("jade.horseStat.jump", showMax, jumpHeight, MAX_JUMP_HEIGHT));
+		tooltip.add(switchText("jade.horseStat.speed", showMax, speed, MAX_MOVEMENT_SPEED));
 	}
 
 	@Override
