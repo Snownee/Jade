a7c6c61aa56583577607d02b6ab570762e501c77
Snownee <i@snownee.com>
2023-08-23 18:42:27 +0800

    improve: make toasts last longer

--- src/main/java/snownee/jade/addon/harvest/HarvestToolProvider.java
+++ src/main/java/snownee/jade/addon/harvest/HarvestToolProvider.java
@@ -21,7 +21,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec2;
 import snownee.jade.api.BlockAccessor;
