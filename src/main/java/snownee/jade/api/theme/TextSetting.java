5a1cc4115fbc7c69ade4b1d41e3ed64397e899e9
Snownee <i@snownee.com>
2023-10-29 10:59:49 +0800

    fix: item count text is black (#324, #326)

--- src/main/java/snownee/jade/api/theme/TextSetting.java
+++ src/main/java/snownee/jade/api/theme/TextSetting.java
@@ -8,7 +8,7 @@ import net.minecraft.network.chat.Style;
 import snownee.jade.api.ui.ColorPalette;
 
 public record TextSetting(ColorPalette colors, boolean shadow, @Nullable Style modNameStyle, int itemAmountColor) {
-	public static final TextSetting DEFAULT = new TextSetting(ColorPalette.DEFAULT, true, Optional.empty(), 0);
+	public static final TextSetting DEFAULT = new TextSetting(ColorPalette.DEFAULT, true, Optional.empty(), 0xFFFFFFFF);
 
 	public TextSetting(ColorPalette colors, boolean shadow, Optional<Style> modNameStyle, int itemAmountColor) {
 		this(colors, shadow, modNameStyle.orElse(null), itemAmountColor);
