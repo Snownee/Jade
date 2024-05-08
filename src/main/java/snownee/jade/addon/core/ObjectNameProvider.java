32f8324458bdc2d2ac2f1038401915287d8c222c
Snownee <i@snownee.com>
2024-05-09 07:36:28 +0800

    improve: use display name on players (#256)

--- src/main/java/snownee/jade/addon/core/ObjectNameProvider.java
+++ src/main/java/snownee/jade/addon/core/ObjectNameProvider.java
@@ -14,6 +14,7 @@ import net.minecraft.world.entity.Display.ItemDisplay;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.ChestBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -50,6 +51,9 @@ public abstract class ObjectNameProvider implements IToggleableProvider {
 				if (stack != null && !stack.isEmpty()) {
 					return stack.getHoverName();
 				}
+			}
+			if (entity instanceof Player) {
+				return entity.getDisplayName();
 			}
 			if (entity instanceof Villager) {
 				return entity.getType().getDescription();
