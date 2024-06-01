87160109ab578379e549d76543bda27803cab0a1
Snownee <i@snownee.com>
2024-06-02 00:53:56 +0800

    fix: miliBucket precision for fluids (close #412)

--- src/main/java/snownee/jade/util/FluidTextHelper.java
+++ src/main/java/snownee/jade/util/FluidTextHelper.java
@@ -1,11 +1,17 @@
 package snownee.jade.util;
 
+import java.text.NumberFormat;
+
 import snownee.jade.api.ui.IDisplayHelper;
 
 public class FluidTextHelper {
 
 	public static String getUnicodeMillibuckets(long amount, boolean simplify) {
-		return IDisplayHelper.get().humanReadableNumber(amount, "B", true);
+		if (amount < 100000) {
+			return NumberFormat.getNumberInstance().format(amount) + "mB";
+		} else {
+			return IDisplayHelper.get().humanReadableNumber(amount, "B", true, null);
+		}
 	}
 
 }
