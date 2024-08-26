1e7213d07887d926c8868bab78ae30707134c16a
Snownee <i@snownee.com>
2024-08-27 06:18:53 +0800

    feat: indicate if signs have wax applied or not (#336)
    # Conflicts:
    #	src/main/resources/assets/jade/lang/en_us.json
    #	src/main/resources/assets/jade/lang/zh_tw.json

--- src/main/java/snownee/jade/impl/ui/CompoundElement.java
+++ src/main/java/snownee/jade/impl/ui/CompoundElement.java
@@ -1,60 +1,34 @@
 package snownee.jade.impl.ui;
 
-import org.jetbrains.annotations.Nullable;
-
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.util.Mth;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.Vec2;
 import snownee.jade.api.ui.Element;
-import snownee.jade.overlay.DisplayHelper;
+import snownee.jade.api.ui.IElement;
 
 public class CompoundElement extends Element {
 
-	private final ItemStack stack;
-	private final float scale;
-	private final String text;
-	public static final CompoundElement EMPTY = new CompoundElement(ItemStack.EMPTY, 1, null);
+	protected final IElement large;
+	protected final IElement small;
 
-	private CompoundElement(ItemStack stack, float scale, @Nullable String text) {
-		this.stack = stack;
-		this.scale = scale == 0 ? 1 : scale;
-		this.text = text;
-	}
-
-	public static CompoundElement of(ItemStack stack) {
-		return of(stack, 1);
-	}
-
-	public static CompoundElement of(ItemStack stack, float scale) {
-		return of(stack, scale, null);
-	}
-
-	public static CompoundElement of(ItemStack stack, float scale, @Nullable String text) {
-		if (scale == 1 && stack.isEmpty()) {
-			return EMPTY;
-		}
-		return new CompoundElement(stack, scale, text);
+	public CompoundElement(IElement large, IElement small) {
+		this.large = large;
+		this.small = small;
 	}
 
 	@Override
 	public Vec2 getSize() {
-		int size = Mth.floor(18 * scale);
-		return new Vec2(size, size);
+		return large.getSize();
 	}
 
 	@Override
 	public void render(GuiGraphics guiGraphics, float x, float y, float maxX, float maxY) {
-		if (stack.isEmpty())
-			return;
-		DisplayHelper.INSTANCE.drawItem(guiGraphics, x + 1, y + 1, stack, scale, text);
-	}
-
-	@Override
-	public @Nullable String getMessage() {
-		if (stack.isEmpty())
-			return null;
-		return "%s %s".formatted(stack.getCount(), stack.getHoverName().getString());
+		Vec2 largeSize = large.getCachedSize();
+		Vec2 smallSize = small.getCachedSize();
+		large.render(guiGraphics, x, y, maxX, maxY);
+		guiGraphics.pose().pushPose();
+		guiGraphics.pose().translate(0, 0, 100);
+		small.render(guiGraphics, x + largeSize.x - smallSize.x, y + largeSize.y - smallSize.y, maxX, maxY);
+		guiGraphics.pose().popPose();
 	}
 
 }
