8bfd784746227fade0eab6fd066f68f9a1d283c7
Snownee <i@snownee.com>
2024-09-07 00:54:06 +0800

    fix: #173

--- src/main/java/mcp/mobius/waila/WailaClient.java
+++ src/main/java/mcp/mobius/waila/WailaClient.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.platform.InputConstants;
 import mcp.mobius.waila.addons.core.CorePlugin;
 import mcp.mobius.waila.api.config.WailaConfig;
 import mcp.mobius.waila.api.config.WailaConfig.DisplayMode;
+import mcp.mobius.waila.compat.JEICompat;
 import mcp.mobius.waila.gui.HomeConfigScreen;
 import mcp.mobius.waila.impl.ObjectDataCenter;
 import mcp.mobius.waila.impl.config.PluginConfig;
@@ -27,6 +28,7 @@ import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import snownee.jade.Jade;
@@ -34,10 +36,14 @@ import snownee.jade.Jade;
 @Mod.EventBusSubscriber(modid = Waila.MODID, value = Dist.CLIENT)
 public class WailaClient {
 
+	private static boolean hasJEI = ModList.get().isLoaded("jei");
+
 	public static KeyMapping openConfig;
 	public static KeyMapping showOverlay;
 	public static KeyMapping toggleLiquid;
 	public static KeyMapping showDetails;
+	public static KeyMapping showRecipes;
+	public static KeyMapping showUses;
 
 	public static void initClient() {
 		ModLoadingContext.get().registerExtensionPoint(ConfigGuiFactory.class, () -> new ConfigGuiFactory((minecraft, screen) -> new HomeConfigScreen(screen)));
@@ -51,6 +57,13 @@ public class WailaClient {
 		ClientRegistry.registerKeyBinding(WailaClient.showOverlay);
 		ClientRegistry.registerKeyBinding(WailaClient.toggleLiquid);
 		ClientRegistry.registerKeyBinding(WailaClient.showDetails);
+
+		if (hasJEI) {
+			showRecipes = new KeyMapping("key.waila.show_recipes", KeyConflictContext.IN_GAME, KeyModifier.NONE, InputConstants.Type.KEYSYM.getOrCreate(323), Jade.NAME);
+			showUses = new KeyMapping("key.waila.show_uses", KeyConflictContext.IN_GAME, KeyModifier.NONE, InputConstants.Type.KEYSYM.getOrCreate(324), Jade.NAME);
+			ClientRegistry.registerKeyBinding(showRecipes);
+			ClientRegistry.registerKeyBinding(showUses);
+		}
 
 		((ReloadableResourceManager) Minecraft.getInstance().getResourceManager()).registerReloadListener(ModIdentification.INSTANCE);
 	}
@@ -77,9 +90,12 @@ public class WailaClient {
 		if (toggleLiquid.isDown()) {
 			Waila.CONFIG.get().getGeneral().setDisplayFluids(!Waila.CONFIG.get().getGeneral().shouldDisplayFluids());
 		}
+
+		if (hasJEI) {
+			JEICompat.onKeyPressed(event);
+		}
 	}
 
-	//public static boolean hasJEI = ModList.get().isLoaded("jei");
 	public static boolean hideModName;
 
 	@SubscribeEvent(priority = EventPriority.LOWEST)
--- src/main/java/mcp/mobius/waila/compat/JEICompat.java
+++ src/main/java/mcp/mobius/waila/compat/JEICompat.java
@@ -1,7 +1,6 @@
 package mcp.mobius.waila.compat;
 
-import com.mojang.blaze3d.platform.InputConstants;
-
+import mcp.mobius.waila.WailaClient;
 import mcp.mobius.waila.api.Accessor;
 import mcp.mobius.waila.impl.ObjectDataCenter;
 import mezz.jei.api.IModPlugin;
@@ -10,33 +9,16 @@ import mezz.jei.api.recipe.IFocus;
 import mezz.jei.api.recipe.IRecipeManager;
 import mezz.jei.api.runtime.IJeiRuntime;
 import mezz.jei.api.runtime.IRecipesGui;
-import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.settings.KeyConflictContext;
-import net.minecraftforge.client.settings.KeyModifier;
-import net.minecraftforge.common.MinecraftForge;
 import snownee.jade.Jade;
 
 @JeiPlugin
 public class JEICompat implements IModPlugin {
 
 	public static final ResourceLocation ID = new ResourceLocation(Jade.MODID, "main");
-	public static KeyMapping showRecipes;
-	public static KeyMapping showUses;
 	private static IJeiRuntime runtime;
-
-	public JEICompat() {
-		if (showRecipes == null) {
-			showRecipes = new KeyMapping("key.waila.show_recipes", KeyConflictContext.IN_GAME, KeyModifier.NONE, InputConstants.Type.KEYSYM.getOrCreate(323), Jade.NAME);
-			showUses = new KeyMapping("key.waila.show_uses", KeyConflictContext.IN_GAME, KeyModifier.NONE, InputConstants.Type.KEYSYM.getOrCreate(324), Jade.NAME);
-			ClientRegistry.registerKeyBinding(showRecipes);
-			ClientRegistry.registerKeyBinding(showUses);
-			MinecraftForge.EVENT_BUS.addListener(JEICompat::onKeyPressed);
-		}
-	}
 
 	@Override
 	public ResourceLocation getPluginUid() {
@@ -48,23 +30,24 @@ public class JEICompat implements IModPlugin {
 		JEICompat.runtime = runtime;
 	}
 
-	private static void onKeyPressed(InputEvent.KeyInputEvent event) {
-		if (runtime == null || showRecipes == null || showUses == null)
+	public static void onKeyPressed(InputEvent.KeyInputEvent event) {
+		if (runtime == null) {
 			return;
-		if (event.getAction() != 1)
+		}
+		if (!WailaClient.showRecipes.isDown() && !WailaClient.showUses.isDown()) {
 			return;
-		if (!showRecipes.isDown() && !showUses.isDown())
-			return;
+		}
 		Accessor<?> accessor = ObjectDataCenter.get();
-		if (accessor == null)
+		if (accessor == null) {
 			return;
+		}
 		ItemStack stack = accessor.getPickedResult();
-		if (stack.isEmpty())
+		if (stack.isEmpty()) {
 			return;
+		}
 
 		IRecipesGui gui = runtime.getRecipesGui();
 		IRecipeManager manager = runtime.getRecipeManager();
-
-		gui.show(manager.createFocus(showUses.isDown() ? IFocus.Mode.INPUT : IFocus.Mode.OUTPUT, stack));
+		gui.show(manager.createFocus(WailaClient.showUses.isDown() ? IFocus.Mode.INPUT : IFocus.Mode.OUTPUT, stack));
 	}
 }
