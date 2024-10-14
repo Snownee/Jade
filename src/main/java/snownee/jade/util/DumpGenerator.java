7206556ae37640303a3ae4cf6da6cf611890502f
Snownee <i@snownee.com>
2024-10-14 22:24:17 +0800

    feat: DumpGenerator: print more info

--- src/main/java/snownee/jade/util/DumpGenerator.java
+++ src/main/java/snownee/jade/util/DumpGenerator.java
@@ -1,7 +1,11 @@
 package snownee.jade.util;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Comparator;
+import java.util.Map;
 
+import net.minecraft.resources.ResourceLocation;
 import snownee.jade.api.IJadeProvider;
 import snownee.jade.impl.WailaClientRegistration;
 import snownee.jade.impl.WailaCommonRegistration;
@@ -14,6 +18,8 @@ public class DumpGenerator {
 		WailaClientRegistration client = WailaClientRegistration.instance();
 		WailaCommonRegistration common = WailaCommonRegistration.instance();
 
+		builder.append("\nGenerated at ").append(ZonedDateTime.now(ZoneOffset.UTC)).append("\n");
+
 		builder.append("\n## Block");
 		createSection(builder, "Icon Providers", client.blockIconProviders);
 		createSection(builder, "Component Providers", client.blockComponentProviders);
@@ -24,7 +30,36 @@ public class DumpGenerator {
 		createSection(builder, "Component Providers", client.entityComponentProviders);
 		createSection(builder, "Data Providers", common.entityDataProviders);
 
+		builder.append("\n## Common Extension");
+		createSection(builder, "Item Storage", common.itemStorageProviders);
+		createSection(builder, "Fluid Storage", common.fluidStorageProviders);
+		createSection(builder, "Energy Storage", common.energyStorageProviders);
+		createSection(builder, "Progress", common.progressProviders);
+
+		builder.append("\n## Client Extension");
+		createSection(builder, "Item Storage", client.itemStorageProviders);
+		createSection(builder, "Fluid Storage", client.fluidStorageProviders);
+		createSection(builder, "Energy Storage", client.energyStorageProviders);
+		createSection(builder, "Progress", client.progressProviders);
+
+		builder.append("\n## Priorities");
+		for (ResourceLocation resourceLocation : common.priorities.getSortedList()) {
+			builder.append("\n* ").append(resourceLocation);
+		}
+
 		return builder.toString();
+	}
+
+	private static void createSection(StringBuilder builder, String subsection, Map<ResourceLocation, ? extends IJadeProvider> map) {
+		if (map.isEmpty()) {
+			return;
+		}
+		builder.append("\n### ").append(subsection);
+		map.forEach((key, value) -> {
+			builder.append("\n\n#### ").append(key);
+			builder.append("\n* ").append(value.getUid()).append(", ").append(value.getClass().getName());
+		});
+		builder.append("\n\n");
 	}
 
 	private static void createSection(StringBuilder builder, String subsection, IHierarchyLookup<? extends IJadeProvider> lookup) {
