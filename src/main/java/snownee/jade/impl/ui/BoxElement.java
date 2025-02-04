189b73587361579292b09c0b369433f5630d1ef9
Snownee <i@snownee.com>
2025-02-05 00:38:21 +0800

    fix: Tooltip animation being weird at uncapped frame rates (close #442)

--- src/main/java/snownee/jade/impl/ui/BoxElement.java
+++ src/main/java/snownee/jade/impl/ui/BoxElement.java
@@ -66,11 +66,15 @@ public class BoxElement extends Element implements IBoxElement {
 				return;
 			}
 			float delta = Minecraft.getInstance().getTimer().getRealtimeDeltaTicks() * 2;
-			if (delta < 1) {
-				diff *= delta;
-			}
-			if (Mth.abs(diff) < 1) {
+			if (delta == 0) {
 				diff = diff > 0 ? 1 : -1;
+			} else {
+				if (delta < 1) {
+					diff *= delta;
+				}
+				if (Mth.abs(diff) < 1) {
+					diff = diff > 0 ? 1 : -1;
+				}
 			}
 			setter.accept((int) (source + diff));
 		} else {
