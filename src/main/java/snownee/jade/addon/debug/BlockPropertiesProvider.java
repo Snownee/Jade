103a7713a35eb576b777ae2074ded936cb641067
LittleCircleOO <lzy6380009@126.com>
2025-04-04 11:54:23 +0800

    add exchangeValue in the tooltip

--- src/main/java/snownee/jade/addon/debug/BlockPropertiesProvider.java
+++ src/main/java/snownee/jade/addon/debug/BlockPropertiesProvider.java
@@ -1,7 +1,12 @@
 package snownee.jade.addon.debug;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.ItemExchangeValue;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FireBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -35,6 +40,19 @@ public enum BlockPropertiesProvider implements IBlockComponentProvider {
 		int burnOdds = ((FireBlock) Blocks.FIRE).getBurnOdds(accessor.getBlockState());
 		if (burnOdds != 0) {
 			tooltip.add(Component.translatable("jade.block_burn_odds", themes.info(burnOdds)));
+		}
+
+		//ONLY FOR CRAFTMINE SNAPSHOT
+		ItemStack stack = accessor.getBlock().asItem().getDefaultInstance();
+		ItemExchangeValue exvComponent = stack.getComponents().get(DataComponents.EXCHANGE_VALUE);
+		Player player = accessor.getPlayer();
+		float originalExchangeValue = 0;
+		if (exvComponent != null) {
+			originalExchangeValue = exvComponent.getValue(player, stack);
+		}
+		double exchangeValue = (double)Math.round((double)originalExchangeValue * (double)1000.0F) / (double)1000.0F;
+		if (exchangeValue != 0){
+			tooltip.add(Component.translatable("item.exchange_value", Component.literal("" + exchangeValue).withStyle(ChatFormatting.WHITE)).withStyle(ChatFormatting.YELLOW));
 		}
 	}
 
