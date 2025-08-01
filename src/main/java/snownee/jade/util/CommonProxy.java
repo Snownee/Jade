b2579df0d7f60cb804a768ce15732d7c0144fb65
Snownee <i@snownee.com>
2025-08-02 07:46:27 +0800

    fix: player inventory is observable

--- src/main/java/snownee/jade/util/CommonProxy.java
+++ src/main/java/snownee/jade/util/CommonProxy.java
@@ -206,7 +206,11 @@ public final class CommonProxy {
 	}
 
 	public static ItemCollector<?> createItemCollector(Accessor<?> accessor, Cache<Object, ItemCollector<?>> containerCache) {
-		if (accessor.getTarget() instanceof AbstractHorseAccess) {
+		Object target = accessor.getTarget();
+		if (target instanceof Player) {
+			return ItemCollector.EMPTY;
+		}
+		if (target instanceof AbstractHorseAccess) {
 			return new ItemCollector<>(new ItemIterator.ContainerItemIterator(
 					o -> {
 						if (o instanceof AbstractHorseAccess horse) {
@@ -215,7 +219,7 @@ public final class CommonProxy {
 						return null;
 					}, 2));
 		}
-		if (!(accessor.getTarget() instanceof ChestBlockEntity)) {
+		if (!(target instanceof ChestBlockEntity)) {
 			try {
 				var storage = findItemHandler(accessor);
 				if (storage != null) {
