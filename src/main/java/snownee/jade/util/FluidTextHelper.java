acf93159a57b9462ab8cce77294ba9cfab2b1e0f
Snownee <i@snownee.com>
2025-10-02 12:59:52 +0800

    fix: fluid text

--- src/main/java/snownee/jade/util/FluidTextHelper.java
+++ src/main/java/snownee/jade/util/FluidTextHelper.java
@@ -1,27 +1,4 @@
 package snownee.jade.util;
-/*
- * MIT License
- *
- * Copyright (c) 2020 Azercoco & Technici4n
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 
 import java.text.NumberFormat;
 import java.util.function.Supplier;
@@ -32,14 +9,8 @@ import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.Component;
 import snownee.jade.JadeClient;
 import snownee.jade.api.ui.NarratableComponent;
-import snownee.jade.overlay.DisplayHelper;
 
-/**
- * A few helpers to display fluids.
- */
 public class FluidTextHelper {
-	public static final long BUCKET = 81000;
-
 	/**
 	 * Return a unicode string representing a fraction, like ¹⁄₈₁.
 	 */
@@ -89,27 +60,8 @@ public class FluidTextHelper {
 		return JadeClient.formatString("narration.jade.N/N", numerator, denominator);
 	}
 
-	public static NarratableComponent getMillibuckets(long droplets, boolean simplify) {
-		long mb = droplets / 81;
-		long leftover = droplets % BUCKET;
-		if (leftover == 0 || droplets >= BUCKET * 100) {
-			String s = DisplayHelper.INSTANCE.humanReadableNumber(mb, "B", true);
-			if (s.endsWith("mB")) {
-				return makeString(s.substring(0, s.length() - 2), mb, 0, 0, "mB");
-			} else {
-				return makeString(s.substring(0, s.length() - 1), mb / 1000L, 0, 0, "B");
-			}
-		}
-		if (droplets % 81 == 0) {
-			return makeString(mb, 0, 0, "mB");
-		}
-		if (simplify) {
-			long g = LongMath.gcd(leftover, BUCKET);
-			if (g >= 1000) {
-				return makeString(mb / 1000, leftover / g, BUCKET / g, "B");
-			}
-		}
-		return makeString(mb, droplets % 81, 81, "mB");
+	public static NarratableComponent getMillibuckets(long mb, boolean simplify) {
+		return makeString(mb, 0, 0, "mB");
 	}
 
 	private static NarratableComponent makeString(long integer, long numerator, long denominator, String unit) {
