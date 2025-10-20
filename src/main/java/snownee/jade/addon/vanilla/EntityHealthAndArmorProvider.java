74802561bb0b085b21c4fdde59794362a5cb658b
Snownee <i@snownee.com>
2025-10-20 08:02:07 +0800

    improve: health display. health now show frozen status and absorption
    (cherry picked from commit 70cd11291b7551d888e89b5998fcf62591b0f008)
    
    # Conflicts:
    #	src/main/resources/jade.accesswidener

--- src/main/java/snownee/jade/addon/vanilla/EntityHealthAndArmorProvider.java
+++ src/main/java/snownee/jade/addon/vanilla/EntityHealthAndArmorProvider.java
@@ -2,8 +2,14 @@ package snownee.jade.addon.vanilla;
 
 import java.util.List;
 
+import org.jetbrains.annotations.Nullable;
+
 import com.google.common.collect.Lists;
 
+import net.minecraft.client.gui.Gui;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.decoration.ArmorStand;
@@ -12,50 +18,35 @@ import snownee.jade.api.EntityAccessor;
 import snownee.jade.api.IEntityComponentProvider;
 import snownee.jade.api.ITooltip;
 import snownee.jade.api.JadeIds;
+import snownee.jade.api.StreamServerDataProvider;
 import snownee.jade.api.config.IPluginConfig;
 import snownee.jade.api.ui.Element;
 import snownee.jade.api.ui.JadeUI;
 import snownee.jade.impl.ui.ArmorElement;
 import snownee.jade.impl.ui.HealthElement;
 
-public class EntityHealthAndArmorProvider implements IEntityComponentProvider {
+public class EntityHealthAndArmorProvider implements StreamServerDataProvider<EntityAccessor, Float> {
 	public static final EntityHealthAndArmorProvider INSTANCE = new EntityHealthAndArmorProvider();
 
 	@Override
-	public void appendTooltip(ITooltip tooltip, EntityAccessor accessor, IPluginConfig config) {
-		boolean healthText = false;
-		boolean armorText = false;
-		List<Element> elements = Lists.newArrayListWithExpectedSize(2);
-		LivingEntity living = (LivingEntity) accessor.getEntity();
-		if (config.get(JadeIds.MC_ENTITY_HEALTH) && isHealthVisible(living)) {
-			float health = living.getHealth();
-			float maxHealth = living.getMaxHealth();
-			HealthElement healthElement = new HealthElement(maxHealth, health);
-			elements.add(healthElement.tag(JadeIds.MC_ENTITY_HEALTH));
-			healthText = healthElement.showText();
-		}
-		if (config.get(JadeIds.MC_ENTITY_ARMOR) && living.getArmorValue() > 0) {
-			ArmorElement armorElement = new ArmorElement(living.getArmorValue());
-			elements.add(armorElement.tag(JadeIds.MC_ENTITY_ARMOR));
-			armorText = armorElement.showText();
-		}
-		if (healthText && armorText) {
-			tooltip.add(elements.get(0));
-			tooltip.append(JadeUI.spacer(4, 0));
-			tooltip.append(elements.get(1));
-		} else {
-			elements.forEach(tooltip::add);
-		}
+	public @Nullable Float streamData(EntityAccessor accessor) {
+		float absorption = ((LivingEntity) accessor.getEntity()).getAbsorptionAmount();
+		return absorption > 0 ? absorption : 0;
+	}
+
+	@Override
+	public StreamCodec<RegistryFriendlyByteBuf, Float> streamCodec() {
+		return ByteBufCodecs.FLOAT.cast();
+	}
+
+	@Override
+	public boolean shouldRequestData(EntityAccessor accessor) {
+		return EntityHealthAndArmorProvider.isHealthVisible((LivingEntity) accessor.getEntity());
 	}
 
 	@Override
 	public ResourceLocation getUid() {
 		return JadeIds.MC_ENTITY_HEALTH;
-	}
-
-	@Override
-	public boolean isRequired() {
-		return true;
 	}
 
 	@Override
@@ -67,4 +58,44 @@ public class EntityHealthAndArmorProvider implements IEntityComponentProvider {
 		return !(entity instanceof ArmorStand || entity instanceof Creaking);
 	}
 
+	public static class Client extends EntityHealthAndArmorProvider implements IEntityComponentProvider {
+		public static final Client INSTANCE = new Client();
+
+		@Override
+		public void appendTooltip(ITooltip tooltip, EntityAccessor accessor, IPluginConfig config) {
+			boolean healthText = false;
+			boolean armorText = false;
+			List<Element> elements = Lists.newArrayListWithExpectedSize(2);
+			LivingEntity living = (LivingEntity) accessor.getEntity();
+			if (config.get(JadeIds.MC_ENTITY_HEALTH) && isHealthVisible(living)) {
+				float health = living.getHealth();
+				float maxHealth = living.getMaxHealth();
+				float absorption = decodeFromData(accessor).orElse(0F);
+				HealthElement healthElement = new HealthElement(
+						living.isFullyFrozen() ? Gui.HeartType.FROZEN : Gui.HeartType.NORMAL,
+						maxHealth,
+						health,
+						absorption);
+				elements.add(healthElement.tag(JadeIds.MC_ENTITY_HEALTH));
+				healthText = healthElement.showText();
+			}
+			if (config.get(JadeIds.MC_ENTITY_ARMOR) && living.getArmorValue() > 0) {
+				ArmorElement armorElement = new ArmorElement(living.getArmorValue());
+				elements.add(armorElement.tag(JadeIds.MC_ENTITY_ARMOR));
+				armorText = armorElement.showText();
+			}
+			if (healthText && armorText) {
+				tooltip.add(elements.get(0));
+				tooltip.append(JadeUI.spacer(4, 0));
+				tooltip.append(elements.get(1));
+			} else {
+				elements.forEach(tooltip::add);
+			}
+		}
+
+		@Override
+		public boolean isRequired() {
+			return true;
+		}
+	}
 }
--- src/main/java/snownee/jade/track/HealthTrackInfo.java
+++ src/main/java/snownee/jade/track/HealthTrackInfo.java
@@ -3,10 +3,13 @@ package snownee.jade.track;
 public class HealthTrackInfo extends TrackInfo {
 	private float health;
 	private float lastHealth;
+	private float absorption;
+	private float lastAbsorption;
 	private int ticksSinceHurt = 1000;
 
-	public HealthTrackInfo(float health) {
+	public HealthTrackInfo(float health, float absorption) {
 		this.health = lastHealth = health;
+		this.absorption = lastAbsorption = absorption;
 	}
 
 	@Override
@@ -18,22 +21,36 @@ public class HealthTrackInfo extends TrackInfo {
 		if (health != lastHealth && ticksSinceHurt >= 5) {
 			lastHealth = health;
 		}
+		if (absorption != lastAbsorption && ticksSinceHurt >= 5) {
+			lastAbsorption = absorption;
+		}
 	}
 
 	public float getLastHealth() {
 		return lastHealth;
 	}
 
+	public float getLastAbsorption() {
+		return lastAbsorption;
+	}
+
 	public boolean isBlinking() {
 		return ticksSinceHurt < 5;
 	}
 
-	public void setHealth(float health) {
+	public void setHealth(float health, float absorption) {
 		if (health < this.health) {
 			this.ticksSinceHurt = 0;
 		} else if (health > this.health) {
 			this.lastHealth = health;
 		}
 		this.health = health;
+
+		if (absorption < this.absorption) {
+			this.ticksSinceHurt = 0;
+		} else if (absorption > this.absorption) {
+			this.lastAbsorption = absorption;
+		}
+		this.absorption = absorption;
 	}
 }
