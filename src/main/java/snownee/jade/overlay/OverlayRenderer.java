b7d5f8ca3ae573e6bd3cd271bf5c48f7cefdfe9f
AmirAli Mollaei <me.amiralimollaei@gmail.com>
2026-01-29 13:41:16 +0330

    disable blend and depth test before matrixStack.popPose()

--- src/main/java/snownee/jade/overlay/OverlayRenderer.java
+++ src/main/java/snownee/jade/overlay/OverlayRenderer.java
@@ -210,16 +210,13 @@ public class OverlayRenderer {
 			callback.afterRender(root, rect, guiGraphics, ObjectDataCenter.get());
 		});
 
-		RenderSystem.enableBlend();
-		RenderSystem.enableDepthTest();
+		RenderSystem.disableBlend();
+		RenderSystem.disableDepthTest();
 		matrixStack.popPose();
 
 		if (Jade.CONFIG.get().getGeneral().shouldEnableTextToSpeech()) {
 			WailaTickHandler.narrate(root.getTooltip(), true);
 		}
-
-		RenderSystem.disableBlend();
-		RenderSystem.disableDepthTest();
 
 		shown = true;
 	}
