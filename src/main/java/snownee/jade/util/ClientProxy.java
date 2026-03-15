8833a1f19ec3457f75ec0ba79b5c5f0782aa8c21
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2026-03-15 03:54:44 -0700

    Fix #704, #734

--- src/main/java/snownee/jade/util/ClientProxy.java
+++ src/main/java/snownee/jade/util/ClientProxy.java
@@ -281,6 +281,11 @@ public final class ClientProxy implements ClientModInitializer {
 		ClientPlayNetworking.send(payload);
 	}
 
+	public static boolean shouldFetchFromServer(@Nullable UUID uuid) {
+		String name = lookupPlayerName(uuid);
+		return name == null || name.equals(PlayerNameLookup.DUMMY_NAME);
+	}
+
 	@Nullable
 	public static String lookupPlayerName(@Nullable UUID uuid) {
 		return PlayerNameLookup.get(uuid, Minecraft.getInstance().services());
