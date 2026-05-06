43128a9e8e1082a8b6f8380ae883394678256254
Patbox <39821509+Patbox@users.noreply.github.com>
2026-05-06 16:30:04 +0200

    Provide packet context for execution (fix #748) (#756)

--- src/main/java/snownee/jade/network/ServerPayloadContext.java
+++ src/main/java/snownee/jade/network/ServerPayloadContext.java
@@ -1,12 +1,13 @@
 package snownee.jade.network;
 
+import net.fabricmc.fabric.api.networking.v1.context.PacketContext;
 import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.server.level.ServerPlayer;
 
 public interface ServerPayloadContext {
 	default void execute(Runnable runnable) {
-		player().level().getServer().execute(runnable);
+		player().level().getServer().execute(() -> PacketContext.runWithContext(this.player(), runnable));
 	}
 
 	default void sendPacket(CustomPacketPayload payload) {
