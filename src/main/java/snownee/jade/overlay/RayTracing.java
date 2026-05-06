1aebfe0edad0b4665e09e28cdb092ea58c083e3b
Snownee <i@snownee.com>
2026-05-06 15:26:47 +0800

    fix: improve compatibility with Create Aeronautics
    (cherry picked from commit a34b15230ccf38d336e6967454be596f51d80389)
    
    # Conflicts:
    #	src/main/java/snownee/jade/overlay/RayTracing.java
    (cherry picked from commit 6f8ec8dbaca5820f83ad359177ee9b4703b9c965)

--- src/main/java/snownee/jade/overlay/RayTracing.java
+++ src/main/java/snownee/jade/overlay/RayTracing.java
@@ -115,7 +115,10 @@ public class RayTracing {
 		boolean startFromEye = IWailaConfig.get().general().getPerspectiveMode() == IWailaConfig.PerspectiveMode.EYE;
 		Vec3 traceStart = startFromEye ? eyePosition : camera.position();
 		double distance = startFromEye ? 0 : eyePosition.distanceToSqr(traceStart);
-		if (distance > 1e-5) {
+		boolean isInvalidHit = mc.hitResult == null;
+		if (distance > 1e8) {
+			isInvalidHit = true;
+		} else if (distance > 1e-5) {
 			distance = Math.sqrt(distance);
 			blockReach += distance;
 			entityReach += distance;
@@ -123,7 +126,7 @@ public class RayTracing {
 
 		Vec3 traceEnd;
 		Vec3 lookVector;
-		if (mc.hitResult == null) {
+		if (isInvalidHit) {
 			lookVector = startFromEye ? entity.getViewVector(partialTick) : new Vec3(camera.forwardVector());
 			traceEnd = traceStart.add(lookVector.scale(entityReach));
 		} else {
