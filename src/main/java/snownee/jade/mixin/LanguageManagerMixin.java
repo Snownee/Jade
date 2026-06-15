bfc8ae127758e78a9393d91e48e21748cac7678f
Snownee <i@snownee.com>
2026-06-15 22:08:29 +0800

    feat: add support for resource packs that use pua characters in the container names (close #770)

--- src/main/java/snownee/jade/mixin/LanguageManagerMixin.java
+++ src/main/java/snownee/jade/mixin/LanguageManagerMixin.java
@@ -1,9 +1,13 @@
 package snownee.jade.mixin;
 
+import java.util.List;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.llamalad7.mixinextras.sugar.Local;
 
 import net.minecraft.client.resources.language.LanguageManager;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -12,7 +16,10 @@ import snownee.jade.util.JadeLanguages;
 @Mixin(LanguageManager.class)
 public class LanguageManagerMixin {
 	@Inject(method = "onResourceManagerReload", at = @At("RETURN"))
-	private void jade$onResourceManagerReload(ResourceManager resourceManager, CallbackInfo ci) {
-		JadeLanguages.INSTANCE.onResourceManagerReload(resourceManager);
+	private void jade$onResourceManagerReload(
+			ResourceManager resourceManager,
+			CallbackInfo ci,
+			@Local(name = "languageStack") List<String> languageStack) {
+		JadeLanguages.INSTANCE.onResourceManagerReload(resourceManager, languageStack);
 	}
 }
