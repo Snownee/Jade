770a164363b4cd143f3c02688ecbf57d53000a0b
Magnus <magnus@magkari.eu>
2026-07-19 13:22:12 +0000

    format

--- src/main/java/snownee/jade/compat/RRVCompat.java
+++ src/main/java/snownee/jade/compat/RRVCompat.java
@@ -1,19 +1,16 @@
 package snownee.jade.compat;
 
+import org.jspecify.annotations.Nullable;
+
 import cc.cassian.rrv.api.ActionType;
 import cc.cassian.rrv.common.overlay.itemlist.view.ItemViewOverlay;
-import org.jspecify.annotations.Nullable;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.ItemStack;
-import snownee.jade.api.JadeIds;
 
 public class RRVCompat implements RecipeLookupPlugin {
-
-	public static final Identifier ID = JadeIds.JADE("main");
-
 	@Override
 	public RecipeLookupResult lookup(ItemStack itemStack, @Nullable Identifier specialId, boolean uses) {
 		return new RecipeLookupResult(
-				"rrv", 0.9f, (s, results) -> ItemViewOverlay.INSTANCE.openRecipeView(itemStack, uses ? ActionType.INPUT : ActionType.RESULT));
+				"rrv", 0.9f, (_, _) -> ItemViewOverlay.INSTANCE.openRecipeView(itemStack, uses ? ActionType.INPUT : ActionType.RESULT));
 	}
 }
