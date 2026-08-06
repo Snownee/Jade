7c24a551b2478235c605e92bea5772c9b7948d45
Snownee <i@snownee.com>
2026-08-07 07:09:55 +0800

    fix: #796
    (cherry picked from commit 9de623e3b578fffc9f7c0eacba89b2758e3a7faf)
    
    # Conflicts:
    #	src/main/java/snownee/jade/util/ClientProxy.java
    (cherry picked from commit 4c8039f76395b4a60f26243afd8e3a27bf7addbd)

--- src/main/java/snownee/jade/util/ClientProxy.java
+++ src/main/java/snownee/jade/util/ClientProxy.java
@@ -129,11 +129,19 @@ public final class ClientProxy {
 	}
 
 	private static void onEntityJoin(EntityJoinLevelEvent event) {
-		DatapackBlockManager.onEntityJoin(event.getEntity());
+		try {
+			DatapackBlockManager.onEntityJoin(event.getEntity());
+		} catch (Throwable e) {
+			WailaExceptionHandler.handleErr(e, null, null);
+		}
 	}
 
 	private static void onEntityLeave(EntityLeaveLevelEvent event) {
-		DatapackBlockManager.onEntityLeave(event.getEntity());
+		try {
+			DatapackBlockManager.onEntityLeave(event.getEntity());
+		} catch (Throwable e) {
+			WailaExceptionHandler.handleErr(e, null, null);
+		}
 	}
 
 	private static void onTooltip(RenderTooltipEvent.GatherComponents event) {
