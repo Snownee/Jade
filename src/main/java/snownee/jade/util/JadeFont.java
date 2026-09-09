c350e606f462cbaf9504e68adba309e4d748bdb7
Snownee <i@snownee.com>
2026-09-10 04:58:12 +0800

    fix: Width of TextElementImpl is miscalculated when Caxton is present
    (close #717)
    
    (cherry picked from commit 9f75d59f40766f4f3bcdb9e9643e7c393c20b5b5)
    (cherry picked from commit 65e280803384548750b49f4625075b4134eea881)

--- src/main/java/snownee/jade/util/JadeFont.java
+++ src/main/java/snownee/jade/util/JadeFont.java
@@ -1,20 +1,56 @@
 package snownee.jade.util;
 
-import net.minecraft.client.StringSplitter;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.font.glyphs.BakedGlyph;
 import net.minecraft.client.gui.font.glyphs.BakedSheetGlyph;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.network.chat.Style;
+import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.util.FormattedCharSink;
+import net.minecraft.util.Mth;
+import net.minecraft.util.StringDecomposer;
+import org.apache.commons.lang3.mutable.MutableFloat;
 
 public class JadeFont extends Font {
 	public JadeFont(Font font) {
 		super(font.provider);
-		this.splitter = new StringSplitter((i, style) -> {
-			BakedGlyph glyph = getGlyphSource(style.getFont()).getGlyph(i);
+		this.splitter = font.splitter;
+	}
+
+	@Override
+	public int width(String text) {
+		return width(splitter.stringWidth(text), filteredWidth(s -> StringDecomposer.iterateFormatted(text, Style.EMPTY, s)));
+	}
+
+	@Override
+	public int width(FormattedText text) {
+		return width(splitter.stringWidth(text), filteredWidth(s -> StringDecomposer.iterateFormatted(text, Style.EMPTY, s)));
+	}
+
+	@Override
+	public int width(FormattedCharSequence text) {
+		return width(splitter.stringWidth(text), filteredWidth(text::accept));
+	}
+
+	private int width(float width, float filteredWidth) {
+		return Math.max(0, Mth.ceil(width - filteredWidth));
+	}
+
+	private float filteredWidth(IteratingSink iterating) {
+		MutableFloat width = new MutableFloat();
+		iterating.accept((index, style, codePoint) -> {
+			BakedGlyph glyph = getGlyphSource(style.getFont()).getGlyph(codePoint);
 			if (isFilteredGlyph(glyph, lineHeight)) {
-				return 0;
+				width.add(glyph.info().getAdvance(style.isBold()));
 			}
-			return glyph.info().getAdvance(style.isBold());
+			return true;
 		});
+		return width.floatValue();
+	}
+
+	@FunctionalInterface
+	private interface IteratingSink {
+		void accept(FormattedCharSink sink);
 	}
 
 	public static boolean isFilteredGlyph(BakedGlyph glyph, int lineHeight) {
